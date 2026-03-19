f0359ae8d0325a81453e8b2ec0c7c1b7fb7128af
BlerkCodes <jc.kazuo@gmail.com>
2026-03-18 21:21:30 -0400

    Added audio functionality
    Added changes to audiomixer and new script volume settings

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -10,6 +10,21 @@ public class AudioManager : MonoBehaviour
     public AudioClip background;
     public AudioClip SomeRandomAction;
 
+    private static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void Start()
     {
         musicSource.clip = background;
